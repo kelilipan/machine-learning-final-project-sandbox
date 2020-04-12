81fa34fabf9a66afbe03eaf0529b344bc921ae79
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-04-12 18:01:03 +0700

    new kMeans function

--- v2/keras.py
+++ v2/keras.py
@@ -44,7 +44,7 @@ model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
               
-model.fit(X_train, y_train, epochs=10, batch_size=1)
+model.fit(X_train, y_train, epochs=500, batch_size=1)
 test_loss, test_acc = model.evaluate(X_test, y_test)
 classes = d['best_pos'].classes_
 print(*zip(range(8),classes),sep='\n')
